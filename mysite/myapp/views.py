4480e082f46cb9af5c3fbc2634590c9e9734fc28
Saifcodex <saifahmed3693@gmail.com>
2025-04-08 00:49:16 +0600

    Add Small functions like home , about , contact , faq in views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from .models import *
+from .forms import  UserProfileForm, UserForm  , RegisterForm , LoginForm
+from django.contrib.auth import (
+    authenticate,
+    login as auth_login,
+    logout as auth_logout
+)
+from django.contrib import messages
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+    render
+)
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def faq(request):
+    return render(request, 'faq.html')
