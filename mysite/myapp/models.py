23f48f6ad66bf51c2c40b2635eb02ffea0f7770f
Saifcodex <saifahmed3693@gmail.com>
2025-04-08 00:39:20 +0600

    Add UserProfile models.py

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    def __str__(self):
+        return self.fullname()
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def fullname(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    age = models.IntegerField()
+    address = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=15)
+    GENDER = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER)
