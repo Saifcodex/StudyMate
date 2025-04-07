e3a95b3d1f07ebfd4e383dd264b3162de4812773
Saifcodex <saifahmed3693@gmail.com>
2025-04-08 00:53:58 +0600

    login functions

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -30,3 +30,22 @@ def faq(request):
 
 def custom_error(request, ):
     return render(request, 'error.html')
+
+
+def login(request):
+    form = LoginForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            username = form.cleaned_data["u_name"]
+            password = form.cleaned_data["u_password"]
+            authenticated_user = authenticate(request, username=username, password=password)
+
+            if authenticated_user is not None:
+                auth_login(request, authenticated_user)
+                messages.success(request, f"Welcome, {username}!")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+
+    return render(request, "login.html", {"form": form})
